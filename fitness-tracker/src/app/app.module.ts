19332802e33e32d148075af664c0aa9e3ac89f98
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2024-03-27 15:09:01 +0200

    added login component

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './core/error/error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './user/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ErrorComponent
+    ErrorComponent,
+    NotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
