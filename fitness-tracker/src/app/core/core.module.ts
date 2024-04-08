11dbffc94fabd1a2052c2fed54f46ae640363daa
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2024-04-08 23:04:29 +0300

    added footer and header

--- fitness-tracker/src/app/core/core.module.ts
+++ fitness-tracker/src/app/core/core.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, AboutComponent, ContactUsComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, FooterComponent, AboutComponent]
+  exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
